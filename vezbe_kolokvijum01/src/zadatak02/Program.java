8a9cb8e1639eaaca34d3a8a419c2e191c621ed71
Dušan Simić <dusan.simic1810@gmail.com>
2021-12-02 22:07:58 +0100

    Ispravljen kod za prvi i dodat drugi zadatak

--- vezbe_kolokvijum01/src/zadatak02/Program.java
+++ vezbe_kolokvijum01/src/zadatak02/Program.java
@@ -1,25 +1,25 @@
-package zadatak01;
+package zadatak02;
 
 /*
  * Napisati program koji kreira jedan izvidjacki kamp i 24 izvidjaca. 12 izvi-
- * djaca je definisano pomocu klase Thread kao pozadinski procesi a 12 pomocu
- * interfejsa Runnable kao korisnicki procesi. Kamp nije zaseban proces. (5 poena)
+ * djaca je definisano pomocu klase Thread a 12 pomocu interfejsa Runnable i svi
+ * su korisnicki procesi. Kamp nije zaseban proces. (5 poena)
  * 
  * 12 izvidjaca definisano pomocu klase Thread po pokretanju idu u sumu da traze
- * pecurke, tj. pozivaju metod Suma::traziPecurke 20 puta, i posle svakog poziva
- * donose pecurke koje su pronasli nazad u kamp (Kamp::donesiPecurke). Ako ih
- * neko prekine u potrazi za pecurkama, hitno se vracaju u kamp bez pecuraka i
- * regularno zavrsavaju svoj rad. (5 poena)
+ * pecurke, tj. pozivaju metod Suma::traziPecurke 20 puta, posle cega donose sve
+ * pecurke koje su pronasli nazad u kamp (pomocu metoda Kamp::donesiPecurke).
+ * Ako ih neko prekine u potrazi za pecurkama, hitno se vracaju u kamp bez
+ * pecuraka i regularno zavrsavaju svoj rad. (5 poena)
  * 
  * 12 izvidjaca definisano pomocu interfejsa Runnable po pokretanju idu u sumu
- * da traze drva za korpe, tj. pozivaju metod Suma::traziDrva 25 puta, i posle
- * svakog poziva donose sakupljena drva nazad u kamp (Kamp::donesiDrva). Ako ih
- * neko prekine u potrazi, izvidjaci se ne obaziru na to i regularno zavrsavaju
- * svoj rad kada obave svih 25 potraga. (5 poena)
+ * da traze drva za potpalu, tj. pozivaju metod Suma::traziDrva 25 puta, posle
+ * cega donose sva sakupljena drva nazad u kamp (Kamp::donesiDrva). Ako ih neko
+ * prekine u potrazi, izvidjaci se ne obaziru na to i regularno zavrsavaju svoj
+ * rad kada obave svih 25 potraga. (5 poena)
  * 
  * Sinhronizovati klasu Kamp tako da se ni u kom slucaju ne izgube pecurke ili
- * drva. Takodje, ne dozvoliti da u bilo kom trenutku u kampu bude vise saka
- * pecuraka nego sto je doneto drva za pravljenje korpi. (10 poena)
+ * drva. Takodje, dodati da izvidjaci po povratku u kamp, pre nego sto zavrse
+ * rad, sacekaju da se vrate u kamp i svi ostali izvidjaci. (10 poena)
  * 
  * Obratiti paznju na elegantnost i objektnu orijentisanost realizacije i stil
  * resenja. Za program koji se ne kompajlira, automatski se dobija 0 poena bez
@@ -36,19 +36,16 @@ public class Program {
 		for (int i = 0; i < 12; i++) {
 			pecurkasi[i] = new Pecurkas(kamp, suma);
 			pecurkasi[i].setName(String.format("Pecurkas_%d", i+1));
-			pecurkasi[i].setDaemon(true);
 			pecurkasi[i].start();
 		}
-		
+
 		Thread[] drvari = new Thread[12];
 		for (int i = 0; i < 12; i++) {
 			drvari[i] = new Thread(new Drvar(kamp, suma));
-			drvari[i].setName(String.format("Drvar_%d", i+1));
-			drvari[i].setDaemon(false);
+			drvari[i].setName(String.format("Pecurkas_%d", i+1));
 			drvari[i].start();
 		}
 	}
-
 }
 
 class Pecurkas extends Thread {
@@ -63,15 +60,16 @@ class Pecurkas extends Thread {
 
 	@Override
 	public void run() {
-		try {
-			for (int i = 0; i < 20; i++) {
-				if (interrupted()) {
-					return;
-				}
-
-				kamp.donesiPecurke(suma.traziPecurke());
+		for (int i = 0; i < 20; i++) {
+			if (interrupted()) {
+				kamp.vratiIzvidjaca();
+				kamp.cekajOstale();
+				return;
 			}
-		} catch (InterruptedException e) {}
+			kamp.donesiPecurke(suma.traziPecurke());
+		}
+		kamp.vratiIzvidjaca();
+		kamp.cekajOstale();
 	}
 	
 }
@@ -91,6 +89,8 @@ class Drvar implements Runnable {
 		for (int i = 0; i < 25; i++) {
 			kamp.donesiDrva(suma.traziDrva());
 		}
+		kamp.vratiIzvidjaca();
+		kamp.cekajOstale();
 	}
 	
 }
@@ -99,18 +99,27 @@ class Kamp {
 
 	private int pecurke = 0;
 	private int drva = 0;
+	private volatile int vraceni = 0;
 
-	public synchronized void donesiPecurke(int koliko) throws InterruptedException {
-		while (pecurke + koliko > drva) {
-			System.out.println("Cekam da bude dosta drva.");
-			wait();
-		}
+	public synchronized void donesiPecurke(int koliko) {
 		pecurke += koliko;
 	}
 
 	public synchronized void donesiDrva(int koliko) {
 		drva += koliko;
-		notifyAll();
+	}
+	
+	public synchronized void vratiIzvidjaca() {
+		vraceni++;
+	}
+	
+	private synchronized boolean sviStigli() {
+		return vraceni != 24;
+	}
+	
+	public void cekajOstale() {
+		System.out.printf("%s ceka ostale.\n", Thread.currentThread().getName());
+		while (sviStigli());
 	}
 }
 
